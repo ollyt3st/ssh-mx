9058843c9313522e0647bbc901c85c95d49cfa99
tcatvpn <92326669+tcatvpn@users.noreply.github.com>
2023-05-08 19:07:18 +0200

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -28,8 +28,7 @@ exit 0
 fi
 
 # Download File Ohp
-wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
-unzip ohpserver-linux32.zip
+wget https://raw.githubusercontent.com/tcatvpn/scriptvps/main/ohp/ohpserver
 chmod +x ohpserver
 cp ohpserver /usr/local/bin/ohpserver
 /bin/rm -rf ohpserver*
